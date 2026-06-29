7d7148327120c514a5d1ca4c3155af425ad7217d
Pako2425 <patryk.kocinski340@gmail.com>
2026-06-29 22:22:21 +0200

    Najprostrza możliwa aplikacja sprawdzająca czy biblioteka PixiJS działa. Utworzenie okna z kolorowym tłem w przeglądarce.

--- nurburacing/client/src/main.ts
+++ nurburacing/client/src/main.ts
@@ -1,33 +1,21 @@
-import { Application, Assets, Sprite } from 'pixi.js';
+import { Application } from 'pixi.js'
 
-(async () => {
-  // Create a new application
-  const app = new Application();
+// Główna pętla
+async function main() {
+	
+	// Utworzenie aplikacji pixi.js
+	const app = new Application();
+	
+	// Inicjalizowanie aplikacji
+	await app.init({
+		width: 1024,
+		height: 768,
+		backgroundColor: 0xFF0000
+	});
+	
+	// Dodawanie do DOM-a
+	document.body.appendChild(app.canvas);
 
-  // Initialize the application with a transparent background
-  await app.init({ backgroundAlpha: 0, resizeTo: window });
+}
 
-  // Append the application canvas to the document body
-  document.body.appendChild(app.canvas);
-
-  // Load the bunny texture
-  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-
-  // Create a new Sprite with the texture
-  const bunny = new Sprite(texture);
-
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
-
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add(() => {
-    // Just for fun, let's rotate our bunny over time!
-    bunny.rotation += 0.1;
-  });
-})();
+main();
