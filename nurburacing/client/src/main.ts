6af204fee202735ff8a3fa0bfece7c8e83ef7ff0
Pako2425 <patryk.kocinski340@gmail.com>
2026-07-06 23:27:18 +0200

    Stworzenie klasy kamery z minimalną funkcjonalnością potrzebną do rozgrywki.

--- nurburacing/client/src/main.ts
+++ nurburacing/client/src/main.ts
@@ -1,47 +1,89 @@
-import { Application, Assets, Sprite, Graphics } from 'pixi.js'
+import { Application, Assets, Sprite, Graphics, Container} from 'pixi.js'
+
+
+class Camera {
+	xpos: number;
+	ypos: number;
+	attachedContainer: Container;
+	zoom: number;
+	
+	moveTo(x: number, y: number): void {
+		this.xpos = x;
+		this.ypos = y;
+		this.attachedContainer.position.x = this.xpos;
+		this.attachedContainer.position.y = this.ypos;
+	}
+	
+	move(dx: number, dy: number): void {
+		this.xpos += dx;
+		this.ypos += dy;
+		this.attachedContainer.position.x = this.xpos;
+		this.attachedContainer.position.y = this.ypos;
+	}
+	
+	setZoom(zoomValue: number) {
+		this.zoom = zoomValue;
+		this.attachedContainer.scale.set(this.zoom);
+	}
+	
+	constructor(init_xpos: number = 0, init_ypos: number = 0, attachedContainer: Container) {
+		this.xpos = init_xpos;
+		this.ypos = init_ypos;
+		this.attachedContainer = attachedContainer;
+		
+	}
+};
 
 // Główna pętla
 async function main() {
-	
-	// Utworzenie aplikacji pixi.js
+		
 	const app = new Application();
 	
-	// Inicjalizowanie aplikacji
 	await app.init({
 		width: 1024,
 		height: 768,
 		backgroundColor: 0x11FFBB
 	});
-	
-	// Dodawanie do DOM-a
+
 	document.body.appendChild(app.canvas);
 
-	// Wczytanie tekstury samochodu gracza
-	//const playerCarTexture = await Assets.load('/assets/cars/fwd_car.png');
+	const worldContainer = new Container;
 	
-	// Utworzenie Sprite-a samochodu gracza i wczytanie tekstury
-	//const playerCarSprite = new Sprite(playerCarTexture);
-	
-	//playerCarSprite.width = 240;
-	//playerCarSprite.height = 240;
-	
-	//playerCarSprite.x = 400;
-	//playerCarSprite.y = 300;
-	
-	// app.stage.addChild(playerCarSprite);
-	
-	
-	// Menu główne
-	const w_mainMenu = app.screen.width * 0.7;
-	const h_mainMenu = app.screen.height * 0.7;
-	const mainMenu = new Graphics().rect(
-		( app.screen.width/2 - w_mainMenu/2 ), ( app.screen.height/2 - h_mainMenu/2 ),
-		w_mainMenu, h_mainMenu)
-		.fill(0x0055AA);
+	// tor
+	const worldTexture = await Assets.load('/assets/tracks/test_track.png');
+	const worldSprite = new Sprite(worldTexture);
+	worldSprite.scale = 0.7;
 		
-		
+	worldContainer.addChild(worldSprite);
 	
-	app.stage.addChild(mainMenu);
+		// samochód
+	const playerCarTexture = await Assets.load('/assets/cars/fwd_car.png');
+	const playerCarSprite = new Sprite(playerCarTexture);
+	
+	playerCarSprite.width = 240;
+	playerCarSprite.height = 240;
+	playerCarSprite.x = 400;
+	playerCarSprite.y = 300;
+	
+	worldContainer.addChild(playerCarSprite);
+
+
+	const MainCamera = new Camera(0,0, worldContainer);
+	
+	
+	app.stage.addChild(worldContainer);
+	MainCamera.moveTo(0,0);
+	MainCamera.zoom = 1;
+	app.ticker.add(() => {
+		if(MainCamera.zoom < 10) {
+			MainCamera.zoom += 0.01;
+		}
+		else {
+			MainCamera.zoom = 1;
+		}
+		MainCamera.setZoom(MainCamera.zoom);
+		console.log(MainCamera.zoom);
+	});
 }
 
 main();
